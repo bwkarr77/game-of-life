6e0979b092371dc24526bc7c81edd3ee14e91c1a
Brett Karr <b.karr@polyclip.us>
2020-09-01 20:08:29 -0500

    Updates

---
+++ precache-manifest.06deeb1ec81d92d8d39c4644f395c157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ac071a1d0f60ee357f04e2d7a96469",
+    "url": "/bwkarr77/game-of-life/index.html"
+  },
+  {
+    "revision": "ca76ab0331f76686e222",
+    "url": "/bwkarr77/game-of-life/static/css/main.be63a03a.chunk.css"
+  },
+  {
+    "revision": "b44aedbc23f4b2622ec6",
+    "url": "/bwkarr77/game-of-life/static/js/2.7d76fc7c.chunk.js"
+  },
+  {
+    "revision": "af945f101cccf5f437e55a3044282b78",
+    "url": "/bwkarr77/game-of-life/static/js/2.7d76fc7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca76ab0331f76686e222",
+    "url": "/bwkarr77/game-of-life/static/js/main.21940184.chunk.js"
+  },
+  {
+    "revision": "a8d60223636c6511c4f9",
+    "url": "/bwkarr77/game-of-life/static/js/runtime-main.53a24dfe.js"
+  }
+]);
